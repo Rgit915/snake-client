0f1a1b9be9a84160665ab72ef9c774801f78e055
Rora Alem <rora.goitom@gmail.com>
2023-06-15 10:15:16 -0400

    import connect function using Es6 shorthand syntax

--- play.js
+++ play.js
@@ -1,23 +1,5 @@
-//Import the 'net' module
-const net = require("net");
-
-//establishes a connection with the game server
-const connect = function () {
-const conn = net.createConnection({
-  host : "localhost", // IP address     
-  port : 50541,       //PORT nmuber here
-}); 
-
-//to handle incoming data
-conn.on("data",(data) => {
- console.log("Message from Server:", data);
-});
-
-//interpret incoming data as text
-conn.setEncoding('utf-8');
-return conn;
-};
-
+//Import 'connect' module using ES6 shorthand syntax
+const {connect} = require("./client");
 
 console.log("Connecting .....")
 connect();
