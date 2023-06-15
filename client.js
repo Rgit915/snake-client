107c20373eca7763489b706537ad02c1d21ac5c5
Rora Alem <rora.goitom@gmail.com>
2023-06-15 12:13:34 -0400

    add Name command to send initials to server upon connection

--- client.js
+++ client.js
@@ -11,6 +11,7 @@ const connect = function () {
   //connect event triggered on a connection asap successfully established
   conn.on("connect",() =>{
     console.log("Successfully connected to game server");
+    conn.write("Name: RA");  // send my Initials'RA' to server upon connection
   })
   
   //to handle incoming data
