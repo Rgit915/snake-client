7c8fc557fcf75523462be59ca4776159b30b6c58
Rora Alem <rora.goitom@gmail.com>
2023-06-15 19:19:55 -0400

    import setupInput module

--- play.js
+++ play.js
@@ -1,5 +1,8 @@
 //Import 'connect' module using ES6 shorthand syntax
 const {connect} = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting .....")
 connect();
+
+setupInput();
