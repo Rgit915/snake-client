02d2c021f919101917fc1cd8e0f5c096127a1793
Rora Alem <rora.goitom@gmail.com>
2023-06-15 14:58:58 -0400

    add function setUpInput to handle input

--- play.js
+++ play.js
@@ -3,3 +3,12 @@ const {connect} = require("./client");
 
 console.log("Connecting .....")
 connect();
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin; //allow to listen for keyboard input and react to it
+};
