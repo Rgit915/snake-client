f81bbce15b5a8e912f5044cf4f7f409d31e59694
Rora Alem <rora.goitom@gmail.com>
2023-06-15 09:49:45 -0400

    add client file & move connect function from play file

---
+++ client.js
@@ -0,0 +1,17 @@
+//establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host : "localhost", // IP address     
+    port : 50541,       //PORT nmuber here
+  }); 
+  
+  //to handle incoming data
+  conn.on("data",(data) => {
+   console.log("Message from Server:", data);
+  });
+  
+  //interpret incoming data as text
+  conn.setEncoding('utf-8');
+  return conn;
+  };
+  
