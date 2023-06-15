7ae7936b7fc3ada0da131a3bbee0cdb56f17b29a
Rora Alem <rora.goitom@gmail.com>
2023-06-15 18:19:09 -0400

    add handleUserInput function & check for ctrl+c input

--- play.js
+++ play.js
@@ -13,3 +13,16 @@ const setupInput = function () {
   stdin.on("data", handleUserInput); //event listener for stdin uses handleUserInput function that runs when input from keyboard received
   return stdin; //allow to listen for keyboard input and react to it
 };
+
+//to processes the received user input
+
+const handleUserInput = function (data) {
+  // \u0003 maps to ctrl+C input
+if (data === '\u0003') {
+  process.exit();  //terminate the program if 'ctrl + C' keypress
+}
+  //if other than 'ctrl + C' pressed ,process the user input
+ console.log("Received input: ", data)
+};
+
+setupInput(); //calling the function
