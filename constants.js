824e5983d7c026c7d93698f51612352c4be7d231
Rora Alem <rora.goitom@gmail.com>
2023-06-17 16:55:52 -0400

    define constants for movement keys, initials and message mappings

--- constants.js
+++ constants.js
@@ -2,6 +2,23 @@
 const IP = "localhost";
 const PORT = 50541;
 
+//store the movement keys in constants
+const MOVE_UP_KEY = 'W';
+const MOVE_LEFT_KEY = 'A';
+const MOVE_DOWN_KEY = 'S';
+const MOVE_RIGHT_KEY = 'D';
+const SEND_MESSAGE_KEY = 'M'
+
+//store the player's name/Initials
+const playerName = 'RA';
+
+//define the message mappings as an object
+const messageMappings = {
+  'H': `Hello from ${playerName}!`,
+  'J': 'love this game!',
+  'K': 'Good luck!',
+
+};
 //export
 module.exports = {
   IP,
