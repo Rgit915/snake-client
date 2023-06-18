8b63037d00d736aca2988464d8f4da5fccacaf6e
Rora Alem <rora.goitom@gmail.com>
2023-06-17 21:48:56 -0400

    updated file

--- constants.js
+++ constants.js
@@ -7,20 +7,27 @@ const MOVE_UP_KEY = 'W';
 const MOVE_LEFT_KEY = 'A';
 const MOVE_DOWN_KEY = 'S';
 const MOVE_RIGHT_KEY = 'D';
-const SEND_MESSAGE_KEY = 'M'
+const SEND_MESSAGE_KEY = 'M';
 
 //store the player's name/Initials
 const playerName = 'RA';
 
 //define the message mappings as an object
 const messageMappings = {
-  'H': `Hello from ${playerName}!`,
-  'J': 'love this game!',
-  'K': 'Good luck!',
+  'H' : 'Hello!',
+  'J' : 'love this game!',
+  'K' : 'Good luck!',
 
 };
 //export
 module.exports = {
   IP,
   PORT,
+  playerName,
+  MOVE_UP_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_RIGHT_KEY,
+  SEND_MESSAGE_KEY,
+  messageMappings,
 };
