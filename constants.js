50a754c4b232150db59654b662698a4fcc61de15
Rora Alem <rora.goitom@gmail.com>
2023-06-19 18:59:42 -0400

    define movements as an obj & change message keys tolowerCase

--- constants.js
+++ constants.js
@@ -2,21 +2,22 @@
 const IP = "localhost";
 const PORT = 50541;
 
-//store the movement keys in constants
-const MOVE_UP_KEY = 'W';
-const MOVE_LEFT_KEY = 'A';
-const MOVE_DOWN_KEY = 'S';
-const MOVE_RIGHT_KEY = 'D';
-const SEND_MESSAGE_KEY = 'M';
+//define movements as an object
+const movements = {
+  w: "Move: up",
+  a: "Move: left",
+  s: "Move: down",
+  d: "Move: right",
+}
 
 //store the player's name/Initials
 const playerName = 'RA';
 
 //define the message mappings as an object
 const messageMappings = {
-  'H' : 'Hello!',
-  'J' : 'love this game!',
-  'K' : 'Good luck!',
+  'h' : 'Hello!',
+  'j' : 'love this game!',
+  'k' : 'Good luck!',
 
 };
 //export
@@ -24,10 +25,6 @@ module.exports = {
   IP,
   PORT,
   playerName,
-  MOVE_UP_KEY,
-  MOVE_DOWN_KEY,
-  MOVE_LEFT_KEY,
-  MOVE_RIGHT_KEY,
-  SEND_MESSAGE_KEY,
+  movements,
   messageMappings,
 };
